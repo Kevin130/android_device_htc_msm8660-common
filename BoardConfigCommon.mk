cd5cc186a501f81e58c158680b8e3234e9755b8a
Dorian Snyder <dastin1015@gmail.com>
2012-10-29 19:13:35 -0700

    msm8660-common: add legacy sta events

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -71,6 +71,7 @@ TARGET_USES_C2D_COMPOSITION := true
 BOARD_EGL_CFG := device/htc/msm8660-common/configs/egl.cfg
 
 # Wifi related defines
+BOARD_LEGACY_NL80211_STA_EVENTS  := true
 WIFI_BAND                        := 802_11_ABG
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
@@ -82,6 +83,7 @@ WIFI_DRIVER_FW_PATH_STA          := "/vendor/firmware/fw_bcmdhd.bin"
 WIFI_DRIVER_FW_PATH_AP           := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_P2P          := "/vendor/firmware/fw_bcmdhd_p2p.bin"
 WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
+
 
 # Webkit
 ENABLE_WEBGL := true
