fe5f9717b0d6eaf7234c9fa2fb2dffc13a565fa8
Dorian Snyder <dastin1015@gmail.com>
2012-10-31 16:00:18 -0700

    msm8660-common: remove camera stub from msm8660-common

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-USE_CAMERA_STUB := true
-
 TARGET_SPECIFIC_HEADER_PATH := device/htc/msm8660-common/include
 
 # Bootloader
